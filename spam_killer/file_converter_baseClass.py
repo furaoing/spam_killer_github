5baccc41c5bc21af59906287c47a5ea5e875d085
TaikorRoy <furaoing@qq.com>
2015-04-17 11:28:40 +0800

    v_0.2_
    aaa

---
+++ spam_killer/file_converter_baseClass.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 15 17:01:29 2015
+
+@author: Taikor
+"""
+
+
+import xlrd
+
+
+class converter:
+    
+    EXCEL = 'excel'
+    TXT = 'txt'
+    CSV = 'csv'
+    # define Marcos here
+    
+    def __init__(self, file_path, from_, to_):
+        self.file_path = file_path
+        self.from_ = str.lower(from_)
+        self.to_ = str.lower(to_)
+        
+    def xls_to_TABtxt_converter(self):
+        workbook = xlrd.open_workbook(self.file_path)
+        sheet = workbook.sheet_by_index(0)    # retrive the very first sheet in excel file
+        nrows = sheet.nrows
+        ncols = sheet.ncols
+
+        lines = list()        
+        for row in range(nrows):
+            line = ""
+            for col in range(ncols):
+                line = line + str(sheet.cell_value(nrows, ncols)) + "/t"
+            line = line[:-1]   # remove the a special character: /t
+            line = line + '/n' # add the a special character: /n
+            lines.append(line)
+        str_lines = "".join(lines)
+        return str_lines        
+        
+            
+    def __call__(self):
+        if (self.from_ is converter.EXCEL) and (self.to_ is converter.TXT):
+            txt_str = self.xls_to_TABtxt_converter()
+            
+        filted_basename = '(filted) ' + retrive_basename(self.file_path)
+        filted_dirname = retrive_dirname(self.file_path)
+        path = filted_dirname + '\\' + filted_basename
+            
+        with open('obj.txt', 'w') as f:
+            f.write(txt_str)
