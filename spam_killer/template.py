8ff5c5bc22fa34ec2894d3b6e71b9bd89c29fbbf
TaikorRoy <furaoing@qq.com>
2015-04-23 17:37:35 +0800

    v0.4.1
    changed sample fold to sample blacklist

---
+++ spam_killer/template.py
@@ -0,0 +1,6 @@
+__author__ = 'Taikor'
+
+
+file_path = "C:\workspace\Github_MasterRepository\spam_killer_github\spam_killer\Debug\汽车3.xlsx"
+blackList_file_path = 'C:\workspace\Github_MasterRepository\spam_killer_github\spam_killer\Debug\kw.txt'
+easy(file_path, blackList_file_path)
