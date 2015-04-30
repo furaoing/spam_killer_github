03b41e4a9b709a566b9a1fdaa5ec8e0861b2d38f
TaikorRoy <furaoing@qq.com>
2015-04-30 15:52:49 +0800

    v0.4.2
    v0.4.2

--- spam_killer/template.py
+++ spam_killer/template.py
@@ -1,5 +1,7 @@
 __author__ = 'Taikor'
 
+from spam_killer import easy
+
 
 file_path = "C:\workspace\Github_MasterRepository\spam_killer_github\spam_killer\Debug\汽车3.xlsx"
 blackList_file_path = 'C:\workspace\Github_MasterRepository\spam_killer_github\spam_killer\Debug\kw.txt'
